7fffe7ce3c70f90e66d4e3537641601124728802
Jordan Ewings <jordanewings@outlook.com>
2023-07-25 22:19:17 -0500

    org

--- source/views.py
+++ source/views.py
@@ -19,13 +19,17 @@ cs = ChatSession()
 
 # main ---------------------------
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
+    return render_template("index.html")
+
+@app.route("/chatcompletion", methods=["GET", "POST"])
+def chatcompletion():
     if request.method == "POST":
         cs.add("user", request.form["query"])
         cs.submit()
-        return redirect(url_for('index'))
-    return render_template("index.html", messages=cs.messages, n=len(cs.messages))
+        return redirect(url_for('chatcompletion'))
+    return render_template("chatcompletion.html", messages=cs.messages, n=len(cs.messages))
 
 @app.route("/api", methods=["GET"])
 def api():
